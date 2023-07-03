b9dd5bd71a7c58526eb16cc1d091feec8e6f77ce
kunalkatara12 <kunalkatara888@gmail.com>
2023-07-03 17:15:52 +0530

    add link

--- Set Matrix Zeroes(1).cpp
+++ Set Matrix Zeroes(1).cpp
@@ -1,4 +1,5 @@
 /*
+Link: https://leetcode.com/problems/set-matrix-zeroes/
 73. Set Matrix Zeroes
 Given an m x n integer matrix matrix, if an element is 0, set its entire row and column to 0's. You must do it in place.
 
